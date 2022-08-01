9cb5c3a5eeb806c861dd2a43988f0b2eed23dcd3
yscho010612@gmail.com <yscho010612@gmail.com>
2022-08-01 11:12:06 +0900

    Implicit Intent test

--- app/src/main/java/mediapoint/project/mirim_project_0801_implicitltent/MainActivity.java
+++ app/src/main/java/mediapoint/project/mirim_project_0801_implicitltent/MainActivity.java
@@ -2,6 +2,7 @@ package mediapoint.project.mirim_project_0801_implicitltent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -39,14 +40,22 @@ public class MainActivity extends AppCompatActivity {
                     intent = new Intent(Intent.ACTION_VIEW, uri);
                     break;
                 case R.id.btn_map:
+                    uri = Uri.parse("http://maps.google.co.kr/maps?q=33.4242095,126.931111&z=15");
+                    intent = new Intent(Intent.ACTION_VIEW,uri);
                     break;
                 case R.id.btn_search:
+                    intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                    intent.putExtra(SearchManager.QUERY, "미림여자정보과학고");
                     break;
                 case R.id.btn_sms:
+                    uri = uri.parse("smsto:"+Uri.encode("010-3333-7777"));
+                    intent = new Intent(Intent.ACTION_SENDTO);
+                    intent.putExtra("sms_body","안녕하세요");
+                    intent.setData(uri);
                     break;
                 case R.id.btn_camera:
+                    intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     break;
-
             }
             startActivity(intent);
         }
